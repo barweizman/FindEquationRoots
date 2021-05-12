7066613dfb6bb1618fc4ba364c1173108c66cca6
BarWeizman <bar232123@gmail.com>
2021-05-12 17:23:34 +0300

    fixed finding even root

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ def Bisection_Method(f, start_point, end_point, e):
 def Check_0(f):
     if f(0) == 0:
         return {0: 0}
+    #check if none?
 
 def Newton_Raphson(f, start_point, end_point, e):
     pass
@@ -50,9 +51,13 @@ def CalcRoots():
             x1 = x2
             x2 = x1 + 0.1
 
+        if f(x2) == 0:  # holds the issue when there is a sign change before and after the 0
+            roots.update({x2: 0})
+
         if f(x1)*f(x2) < 0:  # if there is a sign change
             if choice == '1':
-                roots.update(Check_0(f))  # 0 can be a root, and is a special check
+                if Check_0(f) is not None:
+                    roots.update(Check_0(f))  # 0 can be a root, and is a special check
                 roots.update(Bisection_Method(f, x1, x2, epsilon))  # roots that are crossover points(function)
                 point = Bisection_Method(fprime(f), x1, x2, epsilon)  # roots that are touching points(function-prime)
                 c, iterations = list(point.items())[0]
@@ -66,11 +71,13 @@ def CalcRoots():
 
     # printing all roots , with thier number of iterations
     for root, iterations in roots.items():
-        print("The root is:", root, "| number of iterations:", iterations)
+        print("The root is:", round(root, 6), "| number of iterations:", iterations)
 
 
 def f(x):
-    return x ** 4 + x ** 3 - 3 * (x ** 2)
+    return x ** 2 - 2 * x + 1
+
+# return x ** 4 + x ** 3 - 3 * (x ** 2)
 
 
 def fprime(f):
